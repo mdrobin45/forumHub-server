1494ab674cdb2c6eb0792e5b37d65e7e5535c0e4
Md Robin Rana <developer.smrobin45@gmail.com>
2023-11-30 09:34:42 +0600

    implement block unblock route

--- src/configurations/database.js
+++ src/configurations/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const database = () => {
    try {
-      mongoose.connect(process.env.MONGO_LOCAL_URI);
+      mongoose.connect(process.env.DB_CLOUD_URI);
       console.log("Database connection successful");
    } catch {
       console.log("Database connection failed");
